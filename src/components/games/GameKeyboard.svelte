149c29781ae653db305517d423197c2ced6fb3e4
O'Dell Obrien <dashhax@gmail.com>
2023-01-26 09:12:10 +0800

    Separated keyboard component

---
+++ src/components/games/GameKeyboard.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+	import { createEventDispatcher, onMount } from "svelte";
+
+    const dispatch = createEventDispatcher();
+    const KEYBOARD = [
+        "QWERTYUIOP",
+        "ASDFGHJKL",
+        "ZXCVBNM",
+        " "
+    ]
+
+    function handleReset() {
+        dispatch("clearall");
+    }
+
+    function handleDelete() {
+        dispatch("deletechar");
+    }
+
+    function addChar(char:string) {
+        dispatch("keypress", { char });
+    }
+
+    function handleKeyPress(e:KeyboardEvent) {
+        const key = e.key.toLowerCase();
+        switch (key) {
+            case "escape": {
+                handleReset();
+                break;
+            };
+            case "backspace": {
+                handleDelete();
+                break;
+            };
+            case "enter": {
+                dispatch("submit");
+                break;
+            };
+            default: {
+                if ("abcdefghijklmnopqrstuvwxyz 0123456789".includes(key)) {
+                    addChar(key);
+                }
+            };
+        }
+    }
+
+    onMount(() => {
+        window.addEventListener("keyup", handleKeyPress);
+
+        return () => {
+            window.removeEventListener("keyup", handleKeyPress);
+        }
+    })
+</script>
+<div class="keyboard">
+    <div class="row">
+        <button class="key" on:click={handleReset} style="flex: 4">
+            Clear All
+        </button>
+        <button class="key" on:click={handleDelete}>
+            Delete
+        </button>
+    </div>
+    {#each KEYBOARD as rows, y (y)}
+        <div class="row">
+            {#each rows.split("") as char, x (x)}
+            <button class="key char" on:click={() => addChar(char)}>
+                {char == " " ? "SPACE" : char}
+            </button>
+        {/each}
+        </div>
+    {/each}
+</div>
+<style lang="scss">
+    .keyboard {
+        display: flex;
+        flex-direction: column;
+
+        .row {
+            display: flex;
+        }
+
+        .key {
+            flex: 1;
+            padding: 0.5rem;
+            border: 1px solid darkblue;
+            background-color: lightblue;
+        }
+    }
+</style>
--- src/components/games/MainGame.svelte
+++ src/components/games/MainGame.svelte
@@ -1,137 +1,184 @@
 <script lang="ts">
 	import { dogBreeds } from '../../stores/dogs';
-    import api from "../../lib/api";
+	import api from '../../lib/api';
 	import type { GameController } from './types';
+	import GameKeyboard from './GameKeyboard.svelte';
 
 	export let started: boolean = false;
 
-    const KEYBOARD = [
-        "QWERTYUIOP",
-        "ASDFGHJKL",
-        "ZXCVBNM",
-        " "
-    ]
+	let breedImages: string[] = [];
+	let currentBreedName: string = '';
+	let currentBreedNameArray: any[] = [];
+	let enteredChars = '';
 
-    let breedImages:string[] = [];
-    let currentBreedName:string = "";
-    let currentBreedNameArray:any[] = [];
-    let enteredChars = "";
+	function processBreedName() {
+		currentBreedNameArray = currentBreedName.split('').map((c) => ({
+			char: c,
+			visible: false,
+			state: -1
+		}));
+	}
 
-    function processBreedName() {
-        currentBreedNameArray = currentBreedName.split("").map(c => ({
-            char: c,
-            visible: false
-        }))
+	function loadBreedInfo() {
+		const breeds = $dogBreeds;
+		const breedKeys = Object.keys(breeds);
+
+		const idx = Math.floor(Math.random() * breedKeys.length);
+		const breedName = breedKeys[idx];
+		let subBreedName = '';
+
+		if (breeds[breedName].length > 0) {
+			subBreedName = breeds[breedName][Math.floor(Math.random() * breeds[breedName].length)];
+		}
+
+		const url =
+			subBreedName == ''
+				? `breed/${breedName}/images/random/3`
+				: `breed/${breedName}/${subBreedName}/images/random/3`;
+		console.log(url);
+		api.dogApi
+			.get(url)
+			.then((response) => {
+				const { message } = response.data;
+				breedImages = message;
+				currentBreedName = breedName + (subBreedName ? ' ' + subBreedName : '');
+				processBreedName();
+				started = true;
+			})
+			.catch((error) => {
+				alert('Unable to start game! ' + error.message);
+			});
+	}
+
+	function appendChar(char: string) {
+		if (enteredChars.length < currentBreedName.length) {
+			enteredChars = enteredChars + char;
+		}
+	}
+
+	function backspaceChar() {
+		if (enteredChars.length > 0) {
+			enteredChars = enteredChars.slice(0, enteredChars.length - 1);
+		}
+	}
+
+	function handleReset() {
+		enteredChars = '';
+		processBreedName();
+	}
+
+	function checkGame() {
+		let totalCorrect = 0;
+		console.log(
+			currentBreedName,
+			enteredChars,
+			currentBreedName.toLowerCase() == enteredChars.toLowerCase()
+		);
+
+		if (enteredChars.length != currentBreedName.length) {
+			gameController.end(false);
+			return;
+		}
+
+		let chancesChars = currentBreedName.toLowerCase().split('');
+		currentBreedNameArray = currentBreedNameArray.map((item, i) => {
+			const cInput = enteredChars[i].toLowerCase();
+			if (item.char.toLowerCase() == cInput) {
+				item.visible = true;
+				item.state = 2;
+				totalCorrect++;
+			} else {
+				if (chancesChars.indexOf(cInput) > -1) {
+					item.state = 1;
+					chancesChars.splice(chancesChars.indexOf(cInput), 1);
+				} else {
+					item.state = 0;
+				}
+			}
+
+			return item;
+		});
+
+		if (totalCorrect >= currentBreedName.length) {
+			gameController.end(true);
+		}
+	}
+
+	function handleKeyboardKeyPress(e: CustomEvent) {
+		const { char } = e.detail;
+		appendChar(char);
+	}
+
+	function handleKeyboardDelete() {
+		backspaceChar();
+	}
+
+    function handleKeyboardSubmit() {
+        checkGame();
     }
 
-    function loadBreedInfo() {
-        const breeds = $dogBreeds;
-        const breedKeys = Object.keys(breeds);
-
-        const idx = Math.floor(Math.random() * breedKeys.length);
-        const breedName = breedKeys[idx];
-        let subBreedName = "";
-
-        if (breeds[breedName].length > 0) {
-            subBreedName = breeds[breedName][Math.floor(Math.random() * breeds[breedName].length)];
-        }
-
-        const url = subBreedName == "" ? `breed/${breedName}/images/random/3` : `breed/${breedName}/${subBreedName}/images/random/3`;
-        console.log(url);
-        api.dogApi.get(url).then(response => {
-            const { message } = response.data;
-            breedImages = message;
-            currentBreedName = breedName + (subBreedName ? " " + subBreedName : "");
-            processBreedName();
-            started = true;
-        }).catch(error => {
-            alert("Unable to start game! " + error.message);
-        })
-        
-    }
-
-    function checkGame() {
-        let totalCorrect = 0;
-        console.log(currentBreedName, enteredChars, currentBreedName.toLowerCase() == enteredChars.toLowerCase());
-
-        currentBreedNameArray = currentBreedNameArray.map((item, i) => {
-            const cInput = enteredChars[i];
-            if (item.char.toLowerCase() == cInput.toLowerCase()) {
-                item.visible = true;
-                totalCorrect++;
-            }
-
-            return item;
-        });
-
-        if (totalCorrect >= currentBreedName.length) {
-            gameController.end(true);
-        }
-    };
-
-    function handleKeyPress(e:KeyboardEvent) {
-        console.log(e.key);
+    function handleKeyboardClear() {
+        handleReset();
     }
 
 	export const gameController: GameController = {
 		start() {
-            document.body.addEventListener("keypress", handleKeyPress);
-            loadBreedInfo();
-        },
+			loadBreedInfo();
+		},
 		pause() {},
-		end(win:boolean) {
-            if (win) {
-                alert("You're correct!");
-            } else {
-                alert("Try again...");
-            }
-            document.body.removeEventListener("keypress", handleKeyPress);
-        }
+		end(win: boolean) {
+			if (win) {
+				alert("You're correct!");
+			} else {
+				alert('Try again...');
+			}
+		}
 	};
 </script>
 
 <div class="guess-the-doggo-container">
 	{#if started}
 		<div class="dog-pics">
-            {#each breedImages as img, i (i)}
-                <img src={img} alt="Dog image number {i + 1}" class="dog-pic">
-            {/each}
-        </div>
-        <div class="dog-name">
-            {#each currentBreedNameArray as item, i (i)}
-                <div class="letter">
-                    {item.visible ? item.char : ""}
-                </div>
-            {/each}
-        </div>
-        <div class="entered-chars p-4 text-2xl uppercase text-center">
-            {enteredChars}
-        </div>
-        <div class="keyboard">
-            <button class="key" on:click={() => enteredChars = ""}>
-                Clear All
-            </button>
-            {#each KEYBOARD as rows, y (y)}
-                <div class="row">
-                    {#each rows.split("") as char, x (x)}
-                    <button class="key char" on:click={() => {
-                        if (enteredChars.length < currentBreedName.length) {
-                            enteredChars += char;
-                        }
-                    }}>
-                        {char == " " ? "SPACE" : char}
-                    </button>
-                {/each}
-                </div>
-            {/each}
-        </div>
-        <button class="start-btn bg-blue-400 text-white border-blue-700 rounded-lg py-2 px-10 w-full mt-4" on:click={checkGame}>
-            Check
-        </button>
+			{#each breedImages as img, i (i)}
+				<img src={img} alt="Dog image number {i + 1}" class="dog-pic" />
+			{/each}
+		</div>
+		<div class="dog-name">
+			{#each currentBreedNameArray as item, i (i)}
+				<div
+					class="letter"
+					class:green={item.state == 2}
+					class:orange={item.state == 1}
+					class:red={item.state == 0}
+				>
+					{item.visible ? item.char : ''}
+				</div>
+			{/each}
+		</div>
+		<div class="entered-chars">
+			{enteredChars}
+		</div>
+		<GameKeyboard
+			on:keypress={handleKeyboardKeyPress}
+			on:submit={handleKeyboardSubmit}
+			on:deletechar={handleKeyboardDelete}
+			on:clearall={handleKeyboardClear}
+		/>
+		<button
+			class="start-btn bg-blue-400 text-white border-blue-700 rounded-lg py-2 px-10 w-full mt-4"
+			on:click={checkGame}
+		>
+			Check
+		</button>
 	{:else}
 		<div class="greet-screen p-4">
 			<h3 class="text-3xl text-center mt-16 uppercase greet-h3">Guess The Doggo</h3>
-			<button class="start-btn bg-green-600 text-white border-green-700 rounded-lg" on:click={() => gameController.start()}> Start </button>
+			<button
+				class="start-btn bg-green-600 text-white border-green-700 rounded-lg"
+				on:click={() => gameController.start()}
+			>
+				Start
+			</button>
 		</div>
 	{/if}
 </div>
@@ -139,50 +186,69 @@
 <style lang="scss">
 	.guess-the-doggo-container {
 		min-height: 300px;
-        padding: 1rem;
+		padding: 1rem;
 
-        .keyboard {
-            display: flex;
-            flex-direction: column;
+		.entered-chars {
+			text-transform: uppercase;
+			text-align: center;
+			letter-spacing: 3px;
+			font-size: 1.25rem;
+			padding: 1rem;
+			display: block;
+			min-height: 3.25rem;
+		}
 
-            .row {
-                display: flex;
-            }
+		.dog-name {
+			display: flex;
+			justify-content: space-evenly;
+			height: fit-content;
 
-            .key {
-                flex: 1;
-                padding: 0.5rem;
-                border: 1px solid darkblue;
-                background-color: lightblue;
-            }
-        }
+			.letter {
+				font-size: 2rem;
+				width: 3rem;
+				height: 3rem;
+				display: block;
+				border-radius: 0.5rem;
 
-        .dog-name {
-            display: flex;
-            justify-content: space-evenly;
-            height: fit-content;
+				color: #333;
+				background-color: white;
+				border: 3px solid steelblue;
 
-            .letter {
-                font-size: 2rem;
-                width: 3rem;
-                height: 3rem;
-                display: block;
-                background-color: blanchedalmond;
-                border: 3px solid burlywood;
-                border-radius: 0.5rem;
-            }
-        }
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				text-transform: uppercase;
 
-        .dog-pics {
-            display: flex;
-            justify-content: space-between;
-            padding: 1rem;
+				transition: all 250ms ease;
 
-            .dog-pic {
-                width: 200px;
+				&.red {
+					color: white;
+					background-color: rgb(255, 0, 0);
+					border-color: rgb(170, 0, 0);
+				}
 
-            }
-        }
+				&.orange {
+					background-color: blanchedalmond;
+					border-color: burlywood;
+				}
+
+				&.green {
+					color: white;
+					background-color: rgb(0, 220, 0);
+					border-color: rgb(0, 170, 0);
+				}
+			}
+		}
+
+		.dog-pics {
+			display: flex;
+			justify-content: space-between;
+			padding: 1rem;
+
+			.dog-pic {
+				width: 200px;
+			}
+		}
 
 		.greet-screen {
 			display: flex;
@@ -197,11 +263,11 @@
 				font-size: 1.75rem;
 				text-transform: uppercase;
 				letter-spacing: 4px;
-                border-width: 2px;
-                border-style: solid;
-                width: fit-content;
-                padding: 0.25rem 3rem;
-                align-self: center;
+				border-width: 2px;
+				border-style: solid;
+				width: fit-content;
+				padding: 0.25rem 3rem;
+				align-self: center;
 			}
 		}
 	}
